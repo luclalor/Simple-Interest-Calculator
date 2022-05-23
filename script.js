280274149c8b06e4b02cd734cae8da32169b6c4f
Luc Lalor <Luc.Lalor@ibm.com>
2022-05-23 13:13:11 -0600

    added javascript changes to range input

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value; //retrieve the value from the form for the inputs to the formula
+    var rate = document.getElementById("rate").value; 
+    var years = document.getElementById("Years").value;
     
+    var interest = principal*years*rate/100; //take the values retrieved and execute the simple interest calculation
+    return interest;  //return the output of the simple interest calculation
 }
-        
+
+var futureyear = new Date().getFullYear()+parseInt();
+
+function updateRate(){
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
