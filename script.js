ce0c85d01bfe973a870365283583d5989554fd8d
Luc Lalor <Luc.Lalor@ibm.com>
2022-06-27 20:43:13 -0600

    Changed the logic in the result variable. Removed function call

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
+var futureyear = new Date().getFullYear()+parseInt();
+
 function compute()
 {
     var principal = document.getElementById("principal").value; //retrieve the value from the form for the inputs to the formula
     var rate = document.getElementById("rate").value; 
     var years = document.getElementById("Years").value;
     
-    var interest = principal*years*rate/100; //take the values retrieved and execute the simple interest calculation
-    return output;
+    var amount = principal*years*rate/100; //take the values retrieved and execute the simple interest calculation
+    return amount;
 }
-
-var futureyear = new Date().getFullYear()+parseInt();
 
 function updateRate(){
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
 }
 
-function output(){
-    var result = document.getElementById("result").innerHTML = "If you deposit " console.log(principal) ", at an interest rate of " console.log(rate)
-    "You will receive an amount of "console.log(interest) "in the year "console.log(futureyear)
-    return result;
-}
+var result = document.getElementById("result").innerHTML = "If you deposit "+principal+",\<br>\at an interest rate of "+rate+"%.\<br\>You will receive an amount of "+compute()+"in the year "+futureyear+"."
