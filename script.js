e3dde2686d4387195a37dc7db9df185d1d6040ce
Luc Lalor <Luc.Lalor@ibm.com>
2022-06-27 20:59:29 -0600

    Fixed the bug in the compute function

--- script.js
+++ script.js
@@ -7,12 +7,11 @@ function compute()
     var years = document.getElementById("Years").value;
     
     var amount = principal*years*rate/100; //take the values retrieved and execute the simple interest calculation
-    return amount;
+    var result = document.getElementById("result").innerHTML = "If you deposit "+principal+",\<br>\at an interest rate of "+rate+"%.\<br\>You will receive an amount of "+amount+"in the year "+futureyear+"."
+    return result;
 }
 
 function updateRate(){
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
 }
-
-var result = document.getElementById("result").innerHTML = "If you deposit "+principal+",\<br>\at an interest rate of "+rate+"%.\<br\>You will receive an amount of "+compute()+"in the year "+futureyear+"."
