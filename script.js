3c272fe2fa65e2391b30f3dc35d45879f2845d95
Luc Lalor <Luc.Lalor@ibm.com>
2022-07-22 16:53:45 -0600

    fixed formatting of output in html. fixed the future year function

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function compute()
     var principal = document.getElementById("principal").value; //retrieve the value from the form for the inputs to the formula
     var rate = document.getElementById("rate").value; //get the rate
     var years = document.getElementById("Years").value; //get the years
-    var futureyear = new Date().getFullYear()+parseInt();
+    var futureyear = new Date().getFullYear()+parseInt(years);
     var amount = principal*years*rate/100; //take the values retrieved and execute the simple interest calculation
     var result = document.getElementById("result").innerHTML = "If you deposit "+principal+",\<br>\at an interest rate of "+rate+"%.\<br\>You will receive an amount of "+amount+"$ in the year "+futureyear+"."
     return result;
