76f252e4d2c29f46650a9a062113f7fb54e11154
Luc Lalor <Luc.Lalor@ibm.com>
2022-07-22 16:46:13 -0600

    revising history and restarting assignment from this point

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-var futureyear = new Date().getFullYear()+parseInt();
-
 function compute()
 {
     var principal = document.getElementById("principal").value; //retrieve the value from the form for the inputs to the formula
-    var rate = document.getElementById("rate").value; 
-    var years = document.getElementById("Years").value;
-    
+    var rate = document.getElementById("rate").value; //get the rate
+    var years = document.getElementById("Years").value; //get the years
+    var futureyear = new Date().getFullYear()+parseInt();
     var amount = principal*years*rate/100; //take the values retrieved and execute the simple interest calculation
-    var result = document.getElementById("result").innerHTML = "If you deposit "+principal+",\<br>\at an interest rate of "+rate+"%.\<br\>You will receive an amount of "+amount+"in the year "+futureyear+"."
+    var result = document.getElementById("result").innerHTML = "If you deposit "+principal+",\<br>\at an interest rate of "+rate+"%.\<br\>You will receive an amount of "+amount+"$ in the year "+futureyear+"."
     return result;
 }
 
