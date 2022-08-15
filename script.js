4a47c34f744dc22c2ed9622551a8b348d4be9145
Luc Lalor <Luc.Lalor@ibm.com>
2022-08-14 21:31:31 -0600

    Added validation for the principal input box. but no alert box yet for step 15

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
-function compute()
-{
-    var principal = document.getElementById("principal").value; //retrieve the value from the form for the inputs to the formula
-    var rate = document.getElementById("rate").value; //get the rate
-    var years = document.getElementById("Years").value; //get the years
-    var futureyear = new Date().getFullYear()+parseInt(years);
-    var amount = principal*years*rate/100; //take the values retrieved and execute the simple interest calculation
-    var result = document.getElementById("result").innerHTML = "If you deposit <mark>"+principal+"</mark>,\<br>\at an interest rate of <mark>"+rate+"%</mark>.\<br\>You will receive an amount of <mark>"+amount+"$</mark> in the year <mark>"+futureyear+"</mark>."
-    return result;
+function compute() {
+    a = validateInput(); //validate that the input is a number greater than 0
+    if (a == true){ //if the number is greater than zero, execute the calculation
+        var principal = document.getElementById("principal").value; //retrieve the value from the form for the inputs to the formula
+        var rate = document.getElementById("rate").value; //get the rate
+        var years = document.getElementById("Years").value; //get the years
+        var futureyear = new Date().getFullYear()+parseInt(years);
+        var amount = principal*years*rate/100; //take the values retrieved and execute the simple interest calculation
+        var result = document.getElementById("result").innerHTML = "If you deposit <mark>"+principal+"</mark>,\<br>\at an interest rate of <mark>"+rate+"%</mark>.\<br\>You will receive an amount of <mark>"+amount+"$</mark> in the year <mark>"+futureyear+"</mark>."
+        return result;
+    }
 }
 
+///defining function to validate input
 function validateInput(){
     var input = document.getElementById("principal").value;
-    var isvalid = true;
+    var isvalid = true
     if (isNaN(input) || input < 1){
-        var isvalid = false;
-        var errormessage = document.getElementById("result").innerHTML = "Enter a positive number.";
+        document.getElementById("result").innerHTML = "Enter a positive number.";
+        isvalid = false;
     }
-    return [isvalid, errormessage];
-    }
+        return isvalid ///isvalid would be false if the validation did not pass
+    };
 
 function updateRate(){
     var rateval = document.getElementById("rate").value;
